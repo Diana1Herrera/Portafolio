fc34d0c7a855f2a338170c9d54bf81d3f4013ff7
Diana1Herrera <dianis11_3@hotmail.com>
2025-01-16 19:38:13 -0500

    Cambios en portafolio

---
+++ script.js
@@ -0,0 +1,28 @@
+/*typing animate*/
+
+var typed = new Typed(".typing",{
+    strings:["Desarrolladora Web FullStack","Backend","Frontend",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
+// Maneja el aside
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar elementos
+    const navToggler = document.querySelector(".nav-toggler"); // Botón de hamburguesa
+    const asideMenu = document.querySelector(".aside"); // Menú lateral
+    const navLinks = document.querySelectorAll(".nav li a"); // Enlaces del menú
+
+    // Mostrar/Ocultar el menú al hacer clic en el botón de hamburguesa
+    navToggler.addEventListener("click", () => {
+        asideMenu.classList.toggle("open");
+    });
+
+    // Ocultar el menú al hacer clic en un enlace del menú
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            asideMenu.classList.remove("open");
+        });
+    });
+});
