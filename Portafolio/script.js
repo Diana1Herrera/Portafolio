d6421705c183f1bd068e5aa3378a2ce992d7b5c3
Diana1Herrera <dianis11_3@hotmail.com>
2025-01-16 19:44:06 -0500

    Cambios en portafolio

--- Portafolio/script.js
+++
@@ -1,9 +0,0 @@
-/*typing animate*/
-
-var typed = new Typed(".typing",{
-    strings:["Desarrolladora Web FullStack","Backend","Frontend",],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-
-})
